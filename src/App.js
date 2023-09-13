f28303065d098b736ad77d635f7947e72fd3d481
Alexis Toledo <alexis.tole@gmail.com>
2023-09-13 20:48:05 -0300

    V3.0

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import  NavBar from './components/NavBar/NavBar.js';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
+        <Link to='/'><img src="../assets/LogoR.png" alt="imagelogo" /></Link>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<ItemListContainer  greeting={"Toda la iluminacion para tus espacios en un solo lugar"}/>}/>
           <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
