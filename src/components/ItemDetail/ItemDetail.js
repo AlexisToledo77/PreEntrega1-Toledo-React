40ccc1303792c45a68b36454b74e7177f033418a
Alexis Toledo <alexis.tole@gmail.com>
2023-09-13 23:42:46 -0300

    V3.1

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -24,7 +24,7 @@ const ItemDetail = ({name, img, category, description, price, stock}) => {
                 </p>
             </section>
             <footer className='ItemFooter'>
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('cantidad agregada ',)}/>
+                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log(`Se agregaron '${quantity}`,)}/>
             </footer>
         </article>
     )
